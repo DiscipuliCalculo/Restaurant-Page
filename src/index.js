b3ca2c69b3bddb91780804bec8c8e26ea72c2ca0
Vitam Discipuli <discipulicalculo@gmail.com>
2021-09-29 09:11:12 -0700

    Added new modules

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
+import * as Contact from './contact.js'
+import * as Home from './home.js'
+import * as Menu from './menu.js'
 console.log('loaded')
 
-const pageContent = document.getElementById('content')
-const newDiv = document.createElement("div");
-const h1 = document.createElement('h1');
-const newSpan = document.createElement('span')
-const span2 = document.createElement('span');
-const h1Text = document.createTextNode(`Bill's Burger Bar`)
-
-h1.appendChild(h1Text)
-
-document.getElementById('content').appendChild(newDiv);
-pageContent.lastChild.id = 'navbar';
-document.getElementById('navbar').appendChild(newSpan);
-document.getElementById('navbar').lastChild.textContent = 'Menu';
-document.getElementById('navbar').appendChild(span2);
-document.getElementById('content').appendChild(h1);
